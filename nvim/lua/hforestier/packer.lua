637745536b10f9ec61182a2891ddb7cf2f5a161c
Hugo Forestier <hugo.forestier@legrand.com>
2024-10-25 17:36:37 +0200

    feat: various changes

--- nvim/lua/hforestier/packer.lua
+++ nvim/lua/hforestier/packer.lua
@@ -13,13 +13,13 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+-- use({
+--	  'rose-pine/neovim',
+--	  as = 'rose-pine',
+--	  config = function()
+--		  vim.cmd('colorscheme rose-pine')
+--	  end
+-- })
 
   use({
       "folke/trouble.nvim",
@@ -34,43 +34,44 @@ return require('packer').startup(function(use)
   })
 
   use {
-			'nvim-treesitter/nvim-treesitter',
-			run = function()
-				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-				ts_update()
-			end,}
-  use("nvim-treesitter/playground")
-  use("theprimeagen/harpoon")
-  use("theprimeagen/refactoring.nvim")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use("nvim-treesitter/nvim-treesitter-context");
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,}
+      use("nvim-treesitter/playground")
+      use("theprimeagen/harpoon")
+      use("theprimeagen/refactoring.nvim")
+      use("mbbill/undotree")
+      use("tpope/vim-fugitive")
+      use("nvim-treesitter/nvim-treesitter-context");
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+      use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v1.x',
+          requires = {
+              -- LSP Support
+              {'neovim/nvim-lspconfig'},
+              {'williamboman/mason.nvim'},
+              {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+              -- Autocompletion
+              {'hrsh7th/nvim-cmp'},
+              {'hrsh7th/cmp-buffer'},
+              {'hrsh7th/cmp-path'},
+              {'saadparwaiz1/cmp_luasnip'},
+              {'hrsh7th/cmp-nvim-lsp'},
+              {'hrsh7th/cmp-nvim-lua'},
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+              -- Snippets
+              {'L3MON4D3/LuaSnip'},
+              {'rafamadriz/friendly-snippets'},
+          }
+      }
 
-  use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
-  use("eandrju/cellular-automaton.nvim")
+      use("folke/zen-mode.nvim")
+      use("github/copilot.vim")
+      use("eandrju/cellular-automaton.nvim")
 
 end)
+
